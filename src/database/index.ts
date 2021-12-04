537b979c55f7ed124d89c8d878a5a343dea0a80c
Jerald <jeraldssw@gmail.com>
2021-12-05 00:23:29 +0800

    Fix a bug where firestore rules does not permit unauthenticated requests

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,16 @@
-import firebase from "firebase/app"
+import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    storageBucket: process.env.NEXT_PUBLIC_PROJECT_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-}
+})
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 export default getFirestore(firebaseApp)
